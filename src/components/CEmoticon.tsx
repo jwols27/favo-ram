79e329558475b5434a6fff7af12ca30a593494a0
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-06-30 18:27:59 -0300

    First steps on Character view

--- src/components/CEmoticon.tsx
+++ src/components/CEmoticon.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import '../styles/emoticon.styles.scss';
 import junoBop from '../shared/assets/junoBop.png';
 
 interface IEmoticonProps {
--- src/templates/Characters.view.tsx
+++ src/templates/Characters.view.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Character } from '../models';
+
 import { CharacterService } from '../shared/services/CharacterService';
+import { Character } from '../models';
+import { CCircularLoading } from '../components';
+import '../styles/character.scss';
 
 const CharactersView = () => {
     const { id } = useParams<'id'>();
@@ -11,18 +14,43 @@ const CharactersView = () => {
         CharacterService.getById(+id).then((res) => {
             if (res instanceof Error) return console.log(res.message);
             setCharacter(res);
-            console.log(res);
         });
     }, []);
+
+    if (!character)
+        return (
+            <div className={'center-box'} style={{ flex: 1 }}>
+                <CCircularLoading additionalClasses={'color-1'} />
+            </div>
+        );
+
     return (
-        <div>
-            {character &&
-                Object.values(character).map((value, index) => {
-                    if (typeof value === 'object') {
-                        return undefined;
-                    }
-                    return <p key={index}>{value}</p>;
-                })}
+        <div id={'character'}>
+            <div
+                className={'character-bg'}
+                style={{ backgroundImage: `url(${character.origin.image})` }}
+            />
+            <div className={'shadow-box bg-color-3'}>
+                <h3>{character.name}</h3>
+                <img height={400} src={character.image} alt={character.name} />
+                <br />
+                <span>{character.desc}</span>
+                <br />
+                <span>{character.origin.name}</span>
+                <br />
+                <div>
+                    {character.tags?.map((tag) => (
+                        <div
+                            className={'tag'}
+                            key={typeof tag === 'number' ? tag : tag.id}
+                        >
+                            <div className={'tag-label'}>
+                                {typeof tag === 'number' ? tag : tag.name}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
